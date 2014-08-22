88ebd0fe1e2577a82bb3ccba1cbe180534e790d5
KubaZ <kuba.zmudzinski@gmail.com>
2014-08-22 14:57:37 +0200

    added js logic for submit button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+(function () {
+    'use strict';
+
+    var emailElement = document.querySelector('#trial-email');
+    var submitButton = document.querySelector('#pakiety .btn-submit');
+    var emailSubject = encodeURIComponent(document.querySelector('#pakiety > h3').innerText);
+    var submitButtonInitialValue = submitButton.getAttribute('href');
+
+    function updateSubmitButton () {
+        var email = encodeURIComponent('Email: ' + emailElement.value);
+        submitButton.setAttribute(
+            'href',
+            submitButtonInitialValue + '?subject=' + emailSubject + '&body=' + email
+        );
+    }
+
+    emailElement.addEventListener('change', updateSubmitButton, false);
+})();
